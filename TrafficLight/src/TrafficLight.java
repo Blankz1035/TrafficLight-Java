1001447704da650fdbe7c3e05192ade790550a03
Andy Blankley <andrew.blankley@hotmail.com>
2016-08-04 20:14:47 +0100

    TrafficLights() class
    Created the TrafficLight constructor with assessors to set lights.
    ChangeState()  method to change light colors.

--- TrafficLight/src/TrafficLight.java
+++ TrafficLight/src/TrafficLight.java
@@ -1,12 +1,57 @@
-import java.awt.color.*;
+import java.awt.Color;
+
 public class TrafficLight {
 	
 	// Instance fields
+	private Bulb[] lights;
 	
 	
 	//Constructors
+	public TrafficLight(){
+		lights = new Bulb[3];
+		lights[0] = new Bulb(true,Color.RED, "Red");
+		lights[1] = new Bulb(false,Color.YELLOW, "Yellow");
+		lights[2] = new Bulb(false,Color.GREEN, "Green");
+		
+	}
 	
 	//assessors
+	public Bulb[] getLights(){
+		return lights;
+	}
+	
+	public void setLights(Bulb[] lights){
+		this.lights = lights;
+	}
+	
+	// algorithms  (step by step process for changing light)
+	
+	public void changeState(){
+		//red - Green
+		if (lights[0].isOn()){
+			lights[0].turnOff();
+			lights[2].turnOn();
+		}
+		
+		//Green -yellow
+		else if(lights[2].isOn()){
+			lights[2].turnOff();
+			lights[1].turnOn();
+		}
+		//yellow to red
+		else if (lights[1].isOn()){
+			lights[1].turnOff();
+			lights[0].turnOn();
+		}
+		
+	}// end of changeState
 	
 	
-}
+} //end Class
+
+
+
+
+
+
+
